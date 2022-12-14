f27ee999d2653df35ce67a194cc8621e2ffab6f4
TBA5854 <adhavan5030@gmail.com>
2022-12-14 07:01:19 +0530

    ng play again added

--- Number guessing.py
+++ Number guessing.py
@@ -1,6 +1,4 @@
 from random import randint
-a=None
-b=None
 def getno(a,l,m,n,p):
 	v=0
 	while a==None or a.isdigit() == False:
@@ -10,29 +8,36 @@ def getno(a,l,m,n,p):
 		v+=1
 	else:
 		a=int(a)
-		return a
-b=getno(b,"upper ", "limit " , "upper " , "limit")
-a=getno(a,"lower ","limit" , "lower " , "limit")
-#Hi guys !
+		return a #Hi guys
 g=0
-r=randint(a,b)
-while True:
+z="yes"
+while z=="yes":
+	if g ==0 :
+		y=0
+		a=None
+		b=None
+		b=getno(b,"upper ", "limit " , "upper " , "limit")
+		a=getno(a,"lower ","limit" , "lower " , "limit")
+		r=randint(a,b)
 	x=None
 	x=getno(x," input "," your"," guess " ,"")
-	while x != r:
+	while x != r and y ==0:
 		if x > r :
 			print("You guessed higher , Try again")
 			g+=1
-			
 		else:
 			print("You guessed lower , Try again")
 			g+=1
-			
 		x=int(input("Enter your guess : "))
 	else :
 		print("You guessed right")
 		g+=1
+		y=1
 		print("You guessed in",g,"tries")
-		print("Thanks for playing , a program by TBA")
-		quit()
+		g=0
+		z=input("Do you want to play agaim ? (yes/no)").lower()
+		while z not in ["yes","no"]:
+			print("Enter a valid input")
+			z=input("Do you want to play agaim ? (yes/no)").lower()
+print("Thanks for playing , a program by TBA")
 		
